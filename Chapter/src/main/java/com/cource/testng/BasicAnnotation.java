546a6b8229ae07314d4d8ca56323292e3e0b3b7c
goodsen <18618411965@163.com>
2018-04-28 15:12:43 +0800

    @BeforeMehod注解的方法将每个测试方法之前运行

--- Chapter/src/main/java/com/cource/testng/BasicAnnotation.java
+++ Chapter/src/main/java/com/cource/testng/BasicAnnotation.java
@@ -1,14 +1,18 @@
 package com.cource.testng;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicAnnotation {
     //testNG最基本的注解，用来把方法标记为测试的一部分
     @Test
-    public void testCase1(){
+    public void testCase1() {
         System.out.println("这是测试1");
-
-        public void
-
     }
+    @BeforeMethod
+    public void beforeMehod(){
+            System.out.println();
+        }
+
+
 }
