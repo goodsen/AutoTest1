e11e01111edc4d4934b628592889d8448f49f3db
guo zhaosen <18618411965@163.com>
2018-04-28 14:32:57 +0800

    idea

---
+++ Chapter/src/main/java/com/cource/testng/BasicAnnotation.java
@@ -0,0 +1,14 @@
+package com.cource.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+    //testNG最基本的注解，用来把方法标记为测试的一部分
+    @Test
+    public void testCase1(){
+        System.out.println("这是测试1");
+
+        public void
+
+    }
+}
